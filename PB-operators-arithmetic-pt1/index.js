c3832137cdf7c7be801b04f2b8fde2db7d15fc70
Matmuret <filippo.moretti6@gmail.com>
2020-05-13 15:45:51 +0200

    thats all

---
+++ PB-operators-arithmetic-pt1/index.js
@@ -0,0 +1,24 @@
+let x = 10;
+console.log(x, typeof(x));
+let y = 20;
+console.log(y, typeof(y));
+console.log(y+x);
+console.log(x-y);
+console.log(y-x);
+console.log(x*y);
+console.log(x/y);
+let z = 10;
+console.log(x*y /z);
+let resultOne = (x*y /z);
+console.log(resultOne);
+let a = 15;
+let b = 9;
+console.log(a/b);
+let c = 20;
+let resultTwo = (a+b) *c;
+console.log(resultTwo);
+console.log(++a)
+console.log(--b)
+let d = (b-a);
+console.log(d+c);
+console.log(resultOne % resultTwo);
