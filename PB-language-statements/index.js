4bd95a18e23c4d7bd2c18141a50feaf58c4f8f66
Matmuret <filippo.moretti6@gmail.com>
2020-05-13 10:04:15 +0200

    my new ex

---
+++ PB-language-statements/index.js
@@ -0,0 +1,7 @@
+console.log("hello world")
+// console.log("this is my first js file")
+
+/* console.log("this is my first js file") */
+
+/* 1.No it doesn't run tho
+2.yes, there is a error called syntax error  */
