c50fd53d215075f9ecac9b472171a2646bf184a3
Matmuret <filippo.moretti6@gmail.com>
2020-05-18 14:39:50 +0200

    07

---
+++ 07-PB-data-types-pt1/Filippo.js
@@ -0,0 +1,22 @@
+//1
+let filippo = "Filippo";
+console.log(filippo);
+//2
+console.log(filippo[filippo.length - 6],filippo[filippo.length - 2])
+//3
+let name = "ciaone";
+//4
+let age = (60);
+//5
+let year = (1960);
+console.log(`${name} is ${age} in ${year}`);
+//6
+let programmingLanguage = "JavaScript";
+//7
+let isFun = "true";
+//8
+console.log(`${programmingLanguage} is fun: ${isFun}`)
+//9
+console.log(programmingLanguage[programmingLanguage.length - 10],programmingLanguage[programmingLanguage.length - 6])
+//10
+console.log(programmingLanguage.length);
