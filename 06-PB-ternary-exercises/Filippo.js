0ead1ea9f5e188fbd09193134506a540b7ff85e8
Matmuret <filippo.moretti6@gmail.com>
2020-05-18 12:59:44 +0200

    06

--- 06-PB-ternary-exercises/Filippo.js
+++ 06-PB-ternary-exercises/Filippo.js
@@ -3,6 +3,8 @@
 let isDog;
 console.log(isDog === true ? "pat, pat" : "find me a dog to pat");
 //or
+isDog === true ? console.log("pat, pat") : console.log("find me a dog to pat");
+//or
 if (isDog === true) {
     isDog === 'pat, pat';
 } else {
@@ -12,6 +14,8 @@ console.log(isDog)
 //2
 let speedCheck;
 console.log(speedCheck > 50 ? "you're going too fast!" : "You're driving below the speed limit, Oma");
+//or
+speedCheck > 50 ? console.log("you're going too fast!") : console.log("You're driving below the speed limit, Oma")
 //or
 if (speedCheck > 50) {
     speedCheck === "you're going too fast!"
@@ -23,6 +27,8 @@ console.log(speedCheck)
 let age;
 console.log(age < 16 ? "serve butter beer" : "serve beer");
 //or
+age < 16 ? console.log("serve butter beer") : console.log("serve beer")
+//or
 if (age < 16) {
     age === "serve butter beer"
 } else {
@@ -32,6 +38,8 @@ console.log(age)
 //4
 let isStudent;
 console.log(isStudent === true ? "Ticket costs €5,00" : "Ticket costs €12,00");
+//or
+isStudent === true ? console.log("Ticket costs €5,00") : console.log("Ticket costs €12,00")
 //or
 if (isStudent === true) {
     isStudent === "Ticket costs €5,00"
@@ -43,6 +51,8 @@ console.log(isStudent)
 let okMarie;
 let cake
 console.log(okMarie === cake ? "Let them eat cake" : "Oh, bother");
+//or
+okMarie === cake ? console.log("Let them eat cake") : console.log("Oh, bother")
 //or
 if (okMarie === cake) {
     okMarie === "Let them eat cake"
