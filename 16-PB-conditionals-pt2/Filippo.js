ce6f363017dfb4eef2824f826f3feb254aad589a
Matmuret <filippo.moretti6@gmail.com>
2020-05-27 11:56:41 +0200

    16

--- 16-PB-conditionals-pt2/Filippo.js
+++ 16-PB-conditionals-pt2/Filippo.js
@@ -15,15 +15,15 @@ let JohnBMI = JohnMass / JohnHeight ** 2;
 
 if (MarkBMI > JohnBMI) {
     let MarkHeiger = `Is Mark's BMI higher than John's one?` + true;
-    console.log(`${MarkHeiger}`);
+    console.log(`${MarkHeiger}`)
 } else {
     let JohnHeiger = `Is Mark's BMI higher than John's one?` + false;
-    console.log(`${JohnHeiger}`);
+    console.log(`${JohnHeiger}`)
 };
 
 //5
 if (MarkBMI > JohnBMI) {
-    console.log(`Mark`);
+    console.log(`Mark`)
 } else {
     console.log(`John`);
 };
