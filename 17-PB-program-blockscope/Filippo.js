263eea343d4bc563624997215f1d608198fef519
Matmuret <filippo.moretti6@gmail.com>
2020-05-27 15:44:11 +0200

    17

---
+++ 17-PB-program-blockscope/Filippo.js
@@ -0,0 +1,32 @@
+//1
+let determiner = 5;
+if (determiner < 0) {
+    let x = "less than 0"
+    console.log(x)
+} else {
+    let x = "Greater or equal to 0"
+    console.log(x)
+};
+
+
+//2
+if (determiner >= 0) {
+    let updater = "Greater or equal to 0"
+    let message = "Positive Integer"
+    console.log(message)
+} else {
+    let message = "Positive Integer"
+    console.log(message)
+};
+
+//3
+console.log(message)
+/*it's not possible to console.log "message" out of
+the Block Scope -curly brackets- cause it has been declared inside 
+the Block Scope -curly brackets-*/  
+
+//4
+/*considering example n2 I couldn't use the ternary option cause we have
+more than one variable depending on just one condition, we can not declare
+these variables outside the Block Scope otherwise they wouldn change depending
+on the if/else condition */
