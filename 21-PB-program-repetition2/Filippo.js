59ea499f20544f860fcd71c0d5a35d9aa2f0cf36
Matmuret <filippo.moretti6@gmail.com>
2020-06-02 15:52:59 +0200

    21

---
+++ 21-PB-program-repetition2/Filippo.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//1
+let i = 0; 
+while (i < 15) {
+    console.log(i);
+    i++; 
+}
+
+//2
+
+
+let x = 1;
+let sum = 0;
+ 
+while(x <= 20) {
+  sum = sum + x;
+  x++;
+  console.log(sum)
+}
+
+//3
+let y = 0;
+do {
+    console.log(y);
+    y++;
+    
+} while (y < 20)
