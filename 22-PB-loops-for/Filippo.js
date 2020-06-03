22854fe63159875146f64e3e0dc601f1c49b6c69
Matmuret <filippo.moretti6@gmail.com>
2020-06-03 12:25:24 +0200

    22+1bonus

--- 22-PB-loops-for/Filippo.js
+++ 22-PB-loops-for/Filippo.js
@@ -1,8 +1,9 @@
-//1
+"use strict";
 
+//1
 for (let i = 0; i <= 20; i++) {
     console.log(i);
-}
+};
 
 //2
 for (let bottles = 1; bottles <= 5; bottles++) {
@@ -11,7 +12,7 @@ for (let bottles = 1; bottles <= 5; bottles++) {
     } else {
         console.log("There is " + bottles + " bottle of beer on the wall.")
     }
-}
+};
 
 //3
 for (let x = 0; x <= 20; x++) {
@@ -21,13 +22,13 @@ for (let x = 0; x <= 20; x++) {
     } else {
         console.log(x + " is odd")
     }
-}
+};
 
 //4
 for (let y = 0; y <= 20; y++) {
     let resoult = y * 9;
     console.log(y + " * 9" + " = " + resoult)
-}
+};
 
 //5
 for (let z = 1; z <= 100; z++) {
@@ -38,10 +39,25 @@ for (let z = 1; z <= 100; z++) {
     } else if (z % 5 === 0) {
         console.log("Buzz")
     }
-}
+};
+
 //6
 for (let m = 1, m1 = 1; m <= 1000; m++, m1++) {
     if (m % 3 === 0 && m1 % 5 === 0) {
         console.log(m + m1)
     }
+};
+
+//8
+
+//4 bonus
+for (let b = 1; b <= 10; b++) {
+    console.log(b) 
+    for (let c = 2; c < 11; c++) {
+        /// nested inside
+        console.log("bonus 4: "+ c*b);
+    }
 }
+
+//8
+
