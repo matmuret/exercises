ea885fa45d5ced8bbf50241316751ce55d3062a4
Matmuret <filippo.moretti6@gmail.com>
2020-06-09 11:45:44 +0200

    28

---
+++ 28-PB-function-scope/Filippo.js
@@ -0,0 +1,30 @@
+//1
+const exponential = (num1, num2) => {
+    console.log(num1 + " and " + num2 + " are " + typeof (num1, num2) + "s");
+    let mult;
+    for (let i = 1; i <= num2; i++) {
+        mult = num1 ** i;
+        console.log(mult);
+    }
+}
+exponential(3, 5)
+
+//2
+let myfav = "Mango"
+const myFavoriteFruit = (myfav) => {
+    console.log(`My favourite fruit is ${myfav}`)
+}
+myFavoriteFruit(myfav)
+
+//3
+const exponent = (num1, num2) => {
+    let result;
+    for (let i = 1; i <= num2; i++) {
+        mult = num1 ** i;
+        console.log(result);
+    }
+}
+exponent(3, 5)
+console.log(result);
+
+// I can't access the variable resoult cause it's declared in a local scope.
