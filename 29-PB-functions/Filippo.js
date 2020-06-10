8255e081eecbfc3faa556c3b55b1f58190175298
Matmuret <filippo.moretti6@gmail.com>
2020-06-10 15:21:53 +0200

    29

--- 29-PB-functions/Filippo.js
+++ 29-PB-functions/Filippo.js
@@ -1,34 +1,28 @@
 'use strict';
 
 //1
+console.log("//1");
 const addUp = (num) => {
     let add = 0;
     for (let i = 1; i <= num; i++) {
-        add = add + i;
+        add = add + i; //or add+= i;
     }
-    console.log(add);
-    return
+    return add;
 };
-addUp(4);
-let addUpReturn = addUp(4);
+console.log(addUp(4));
 
 //2
-const cubed = (num1, num2, num3) => {
-    console.log(num1 * num1 * num1 + num2 * num2 * num2 + num3 * num3 * num3);
-    return;
-}
-cubed(1, 5, 9);
-let cubedReturn = cubed(1, 5, 9);
+console.log("//2");
+const sumOfCubes = (num1 = 0, num2 = 0, num3 = 0) => num1 ** 3 + num2 ** 3 + num3 ** 3;
+console.log(sumOfCubes(2))
 
 //3
-/* const dictionary = (string, word) => {
-    const stringCheck = word.startsWith(string);
-    
-    return;
-}
-console.log (dictionary ("cia", "ciaone")); */
+console.log("//3");
+const dictionary = (string, word) => word.startsWith(string)
 
-const dictionary = (string, word) => {
+console.log(dictionary("cia", "ciaone"));
+
+/* const dictionary = (string, word) => {
     let myString = string
     let myWord = word;
     if (myString === myWord.substring(0, 3)) {
@@ -42,104 +36,125 @@ dictionary("bia", "ciaone");
 const dictionaryReturn = dictionary("bia", "ciaone");
 dictionary("bu", "button");
 dictionary("tri", "triplet");
-dictionary("beau", "pastry");
+dictionary("beau", "pastry"); */
 
 //4
-const lessEqual = (num4) => {
+console.log("//4")
+const lessThanOrEqualToZero = (num4) => num4 <= 0;
+/* const lessEqual = (num4) => {
     if (num4 <= 0) {
-        console.log("ex4 " + true)
+       return true;
     } else {
-        console.log("ex4 " + false)
+        return false;
     }
-    return;
-}
-lessEqual(3);
-const lessEqualReturn = lessEqual(3);
+    
+} */
+console.log(lessThanOrEqualToZero(3))
+console.log(lessThanOrEqualToZero(0))
+console.log(lessThanOrEqualToZero(-4))
+console.log(lessThanOrEqualToZero(10))
 
 //5
-/* const occurences = (stringa, letter) => {
-
+console.log("//5")
+const countOccurrences = (stringa, letter) => {
     let output = 0;
-    for (let i = 0; i <= stringa.length(); i++) {
-        if (stringa[i] === letter) {
-            output += 1
-        }
+    for (let i = 0; i < stringa.length; i++) {
+        if (stringa[i] === letter) //or if (stringa.chartAt(i)===letter.....just for internet explorer so not updated)
+        {
+            output++ // or output += 1
+        };
     }
-    console.log(output);
-    return
+    return output
 };
-occurences("Filippo", "p"); */
+const occurencesR = countOccurrences("filippo", "p");
+console.log(occurencesR)
 
-//6
-
-const calculateBaseToExponent = (base, exponent)=>{  
-    console.log(base** exponent)
-    return;
-}
-calculateBaseToExponent(5, 5);
+//6, 
+console.log("//6")
+const calculateBaseToExponent = (base, exponent) => base ** exponent;
+let calculateBaseToExponent2 = calculateBaseToExponent(5, 5);
+console.log(calculateBaseToExponent2)
 
 //7
-
-const dogYears = (dogY) => {
-    let humanY = dogY*7;
-    console.log("Your doggo is "+ humanY+ " years old in human years!");
-    return 
-}
-dogYears(4)
+console.log("//7")
+const dogAge = (dogY) => dogY * 7;
+let dogYears2 = dogAge(4);
+console.log("Your doggo " + dogYears2 + " years old in human years!")
 
 //8
-const lifetimeSupply =(age, dayAmmount) => {
-    let rest= (80-age)*(365*dayAmmount);
-    console.log("You will need "+ rest +" bars of chocolate (2 a day) to last you til the age of 80.")
-}
-lifetimeSupply(25,2)
+console.log("//8")
+const calcSupply = (age, dayAmmount) => (80 - age) * (365 * dayAmmount);
+const lifetimeSupply2 = calcSupply(25, 2);
+console.log("You will need " + lifetimeSupply2 + " bars of chocolate (2 a day) to last you til the age of 80.")
+
 
 //9
-const isWaldoHere = (stringW) => {
-    if (stringW.includes("Waldo")) {
-        console.log(true)
-    } else {console.log(false)} 
-}
-isWaldoHere("is there wal here ?");
+console.log("//9")
+const isWaldoHere = (stringW) => stringW.includes("Waldo")
+console.log(isWaldoHere("is there wal here ?"));
 
 //10
-const Pie = (sliceN, recipientsN, personN)=>{
-if ((recipientsN*personN)===sliceN) {
-    console.log(true)
-}else {
-    console.log(false)}
-}
-Pie(11, 5, 3)
+console.log("//10");
+const equalSlices = (sliceN, recipientsN, personN) => recipientsN * personN <= sliceN;
+console.log(equalSlices(11, 5, 3));
+console.log(equalSlices(8, 3, 2));
+console.log(equalSlices(8, 3, 3));
+console.log(equalSlices(24, 12, 2));
 
 //11
-/* const XO = (strinX, x, o)=>{
-let outputO = 0;
-let outputX = 0;
-for (let ix = 0; ix <= strinX.length(); ix++) {
-    if (stringX[ix] === x) {
-        outputX += 1
-    }}
-    for (let io = 0; io <= strinX.length(); io++) {
-        if (stringX[io] === o) {
-            outputO += 1
-        }}
-if (outputO===outputX) {
-    return true
-} else {return false}
+console.log("//11");
 
+/* Notes:
+Return a boolean value (true or false).
+The string can contain any character.
+When neither an x nor an o is in the string, return true.
+Examples: */
+
+const xo = (myString) => {
+    let checkForX = 0; // "x"
+    let checkForO = 0; // "o"
+    let myNewString = myString.toLowerCase();
+    for (let position = 0; position < myNewString.length; position++) {
+        if (myNewString.charAt(position) === "x") {
+            checkForX++;
+        } else if (myNewString.charAt(position) === "o") {
+            checkForO++;
+        }
+    }
+    if (checkForO === checkForX) {
+        return true;
+    } else {
+        return false;
+    }
 }
-XO("ooxx") */
+console.log(xo("ooxx")); // ➞ true
+console.log(xo("xooxx")); // ➞ false
+console.log(xo("ooxXm")); // ➞ true (case insensitive)
+console.log(xo("zpzpzpp")); // ➞ true (returns true if no x and o)
+console.log(xo("zzoo")); // ➞ false
 
 //12
-function isPrimeNumber(n) {
-    for (var i = 2; i < n; i++) { // i will always be less than the parameter so the condition below will never allow parameter to be divisible by itself ex. (7 % 7 = 0) which would return true
-      if(n % i === 0) return false; // when parameter is divisible by i, it's not a prime number so return false
+console.log("//12")
+
+function isPrime(n) {
+    for (let i = 2; i < n; i++)
+
+    /* it will always be less than the parameter so the condition below will never allow parameter 
+    to be divisible by itself ex. (7 % 7 = 0) which would return true  */
+
+    {
+        if (n % i === 0) return false;
     }
-    return n > 1; // otherwise it's a prime number so return true (it also must be greater than 1, reason for the n > 1 instead of true)
-  }
-  
-  console.log(isPrimeNumber(1));  // returns false
-  console.log(isPrimeNumber(2));  // returns true
-  console.log(isPrimeNumber(9));  // returns false
-  console.log(isPrimeNumber(11)); // returns true
-    
+
+    // when parameter is divisible by i, it's not a prime number so return false
+
+    return n > 1;
+
+    /* otherwise it's a prime number so return true (it also must be greater than 1, 
+    reason for the n > 1 instead of true) */
+}
+
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(9));
+console.log(isPrime(11));
