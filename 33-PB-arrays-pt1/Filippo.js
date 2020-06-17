b5da4148e5d28beb12e44605da88483c300f4de7
Matmuret <filippo.moretti6@gmail.com>
2020-06-17 15:17:35 +0200

    33 till 11

--- 33-PB-arrays-pt1/Filippo.js
+++ 33-PB-arrays-pt1/Filippo.js
@@ -7,10 +7,10 @@ console.log(secondVar);
 
 //2
 euroCities[0]="Berlin";
-console.log(euroCities);
+console.log("//2 "+euroCities);
 
 //3
-console.log(euroCities.length);
+console.log("//3 "+euroCities.length);
 
 //4
 euroCities.pop();
@@ -18,11 +18,30 @@ euroCities.pop();
 //5
 euroCities.push("Budapest");
 
+//6
+euroCities.splice(1,2);
+console.log("//6 "+euroCities); 
+
 //7
 let asianCities = ["Hong Kong","Pei Jing","Macao","Tokyo","Singapore"];
+
+//8
+let thirdVar = asianCities.slice(1,3);
+console.log("//8 "+thirdVar);
+
+//9
+let wordCities = euroCities.concat(asianCities);
+console.log("//9 "+wordCities);
 
 //10
 asianCities.reverse();
 console.log(asianCities);
+
+//11
+asianCities.splice(2, 1, 'Toronto');
+console.log("/11 "+ asianCities);
+
+
+//12
 
 
