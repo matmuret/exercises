2eb1f525def3127502cb1d75c565278eee4770f2
Matmuret <filippo.moretti6@gmail.com>
2020-06-24 15:45:16 +0200

    38

--- 38-PB-datastructure-objectscope/Filippo.js
+++ 38-PB-datastructure-objectscope/Filippo.js
@@ -21,10 +21,17 @@ const person = {
     method: function (){
         return this.firstName+ " " +this.lastName+ "is a "+this.age+" year old "+this.job+" living in "+this.city;
     },
+
+    /* Object.keys() returns an array whose elements are strings 
+    corresponding to the enumerable properties found directly upon object. 
+    The ordering of the properties is the same as that 
+    given by looping over the properties of the object manually. */
+    
     lengthMethod: function(){
-        return this.person.length();
+        return Object.keys(person).length;
     }
 };
 console.log(person.method());
 
 //bonus
+console.log(person.lengthMethod())
