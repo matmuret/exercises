a2f75763266e20b74b918d18ffd05c22ba90eabf
Matmuret <filippo.moretti6@gmail.com>
2020-06-24 15:54:37 +0200

    38

--- 38-PB-datastructure-objectscope/Filippo.js
+++ 38-PB-datastructure-objectscope/Filippo.js
@@ -1,33 +1,33 @@
 "use strict";
 
 //1
-const student = { 
-    firstName: "John", 
-    lastName: "Smith", 
+const student = {
+    firstName: "John",
+    lastName: "Smith",
     class: 12,
-    method: function (){
-        return this.firstName + " " + this.lastName+ " " +"is a student in class "+ this.class;
+    method: function () {
+        return this.firstName + " " + this.lastName + " " + "is a student in class " + this.class;
     }
 };
 console.log(student.method())
 
 //2
-const person = { 
-    firstName: "John", 
-    lastName: "Smith", 
+const person = {
+    firstName: "John",
+    lastName: "Smith",
     age: 12,
     job: "kid",
     city: "Portomaggiore",
-    method: function (){
-        return this.firstName+ " " +this.lastName+ "is a "+this.age+" year old "+this.job+" living in "+this.city;
+    method: function () {
+        return this.firstName + " " + this.lastName + "is a " + this.age + " year old " + this.job + " living in " + this.city;
     },
 
     /* Object.keys() returns an array whose elements are strings 
     corresponding to the enumerable properties found directly upon object. 
     The ordering of the properties is the same as that 
     given by looping over the properties of the object manually. */
-    
-    lengthMethod: function(){
+
+    lengthMethod: function () {
         return Object.keys(person).length;
     }
 };
