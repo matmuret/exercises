aa717a6c0635e81fd77adeff53f4f12d7ed50939
Matmuret <filippo.moretti6@gmail.com>
2020-07-02 16:04:45 +0200

    45

--- 45-PB-datastructure-classes/Filippo.js
+++ 45-PB-datastructure-classes/Filippo.js
@@ -33,7 +33,7 @@ class CylinderVolume {
         this.h = h
         /* this.p=p */
         this.calcVolume = function () {
-            let result = r * r * Math.PI * h
+            let result = r * r * Math.PI /* p */ * h
             return result.toFixed(4)
         }
 
@@ -42,3 +42,44 @@ class CylinderVolume {
 }
 volumeOne = new CylinderVolume(5, 5);
 console.log("// 2", volumeOne.calcVolume())
+
+/* #### 4. TV Class
+* Create a TV class with properties like brand, channel and volume.
+* Specify brand in a constructor parameter. Channel should be 1 by default. 
+Volume should be 50 by default.
+* Add methods to increase and decrease volume. Volume can't never be below 0 or above 100.
+* Add a method to set the channel randomly. Let's say the TV has only 50 channels.
+* Add a method to reset TV so it goes back to channel 1 and volume 50. 
+* It's useful to write a status, that returns info about the TV status like: "Panasonic at channel 8, 
+volume 75". */
+class TVchannel {
+    constructor(brand = "Panasonic", channel = 1, volume = 50) {
+        this.brand = brand
+        this.channel = 1 //hardcoding
+        this.volume = 50 //hardcoding
+        this.volumeIncrease = function () {
+            if (volume > 0 || volume < 100) {
+                let volumm = volume + 1
+                return `Panasonic at channel ${channel}, volume ${volumm}`
+            } else {
+                return "Error!"
+            }
+        }
+        this.randomChannel = function () {
+            let randomchannel = Math.floor(Math.random() * 50);
+            return `Panasonic at channel ${randomchannel}, volume ${volume}`
+        }
+        this.reset = function () {
+            channel = 1
+            volume = 50
+            return `Panasonic at channel ${channel}, volume ${volume}`
+        }
+
+
+    }
+
+}
+TV = new TVchannel(5, 5);
+console.log("// 4", TV.volumeIncrease())
+console.log("// 4", TV.randomChannel())
+console.log("// 4", TV.reset())
