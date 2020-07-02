2c7ecea039aecef1e1a51a4a9bd2ecf92ba43b62
Matmuret <filippo.moretti6@gmail.com>
2020-07-02 13:59:57 +0200

    45

--- 44-PB-datastructure-logicalthinking/Filippo.js
+++ 44-PB-datastructure-logicalthinking/Filippo.js
@@ -6,8 +6,6 @@ const num = [4, 2, 5, 3, 9, 7, 1, 8, 2, 6];
 const sortNum = num.sort()
 console.log(sortNum);
 
-/* Given an array of names:
-Sort the array based on alphabetical order from `a → z`.  */
 
 const lett=["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"];
 const sortLett=lett.sort();
@@ -16,11 +14,34 @@ console.log(sortLett);
 /* 1. Once you've wrote down your pen and paper solutions, 
 create functions to solve the problems above with javascript.  */
 
+/* Lowest to Highest
+0. Create an empty array
+1. Go through items of given array one after another
+2. Compare items on position 0 with item on position one
+3. Item which is smaller compare with next one
+4. Find the smallest item
+5. Push the smallest item in an empty array on position 0 and delete this one 
+from original array.
+6. Do again steps 2- 5 until original array not will be empty */
+/* Given an array of names:
+Sort the array based on alphabetical order from `a → z`.  */
+
 const sortNumber = (arr) => arr.sort()
 console.log(sortNumber([4, 2, 5, 3, 9, 7, 1, 8, 2, 6]))
 
+/* 
+Alphabetical Order
+0. Create an empty array
+1. Create an array with all letters
+3. Take a first letter of first item in a given array and find an index in array of letters
+4. Take a first letter of second word and find its index in letter array
+5. Compare
+6. One is smaller compare with first letter index of next word
+7. If some items have the same index of first letter continue compare indexes of second letters
+8. When one is left push it an empty array
+9. Delete this one from given array
+10.Continue 3-9 till given array not will be empty */
 
 const sortLetter = (arr) => arr.sort()
 console.log(sortLetter(["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"]))
-
 
---
+++ 45-PB-datastructure-classes/Filippo.js
@@ -0,0 +1,43 @@
+/* #### 1. Person Class
+* Create a class called `Person` which accepts the name of a person as a string, 
+and his/her age as a number. The `Person` class should have a method called 
+describe which returns a string with the following syntax: "name, age years old". 
+So, for example, if John is 19 years old, 
+then the function describe of his object will return "John, 19 years old". */
+
+class Person {
+    constructor(name,age){
+        this.name=name
+        this.age=age
+        this.greeting = function () {
+            return `"${name}, ${age} years old"`;
+        }
+
+    }
+
+}
+const filippo = new Person("Filippo",37);
+console.log(filippo);
+person=new Person("Filippo",37);
+console.log("// 1",person.greeting())
+
+/* #### 2. Volume
+* Write a JavaScript program to get the volume of a cylinder 
+with four decimal places using a class. 
+* **Note**: Volume of a cylinder : `V = πr^2h` - r is the 
+radius and h is the height of the cylinder.
+*/
+class CylinderVolume {
+    constructor(r,h ,p=3.14){
+        this.r=r
+        this.h=h
+        this.p=p
+        this.calcVolume = function () {
+            return (r*r*p)*h
+        }
+
+    }
+
+}
+volumeOne=new CylinderVolume(5,5);
+console.log("// 2",volumeOne.calcVolume())
