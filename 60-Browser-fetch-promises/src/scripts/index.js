a1df7624e29f13dab08e03f602dee1115a4807ca
Matmuret <filippo.moretti6@gmail.com>
2020-08-05 13:53:35 +0200

    60

--- 60-Browser-fetch-promises/src/scripts/index.js
+++ 60-Browser-fetch-promises/src/scripts/index.js
@@ -1,7 +1,26 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+/* import '../styles/main.scss'; */
 // Import any additional modules you want to include below \/
 
 
 // \/ All of your javascript should go here \/
+let modal = document.getElementById("myModal")
+let close = document.querySelector(".close")
 
+let showModal = () => {
+    let promise = new Promise((resolve, reject) => {
+
+        resolve(setTimeout(() => {
+            modal.style.display = "block"
+        }, 1000))
+        reject("err")
+    })
+
+    return promise
+}
+
+showModal()
+    .then()
+    .catch(err => console.log(err))
+
+    close.addEventListener("click",()=>{modal.style.display = "none"})
